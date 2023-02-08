eb990e1e862f2689a6fd75d1684be566b509ce5a
Somewhere <66011798+Kuvaev-dev@users.noreply.github.com>
2023-02-08 20:11:37 +0200

    Layout Template And Database Entities Added

---
+++ CloudERP/Controllers/UserTypesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DatabaseAccess;
+
+namespace CloudERP.Controllers
+{
+    public class UserTypesController : Controller
+    {
+        private CloudDBEntities db = new CloudDBEntities();
+
+        // GET: UserTypes
+        public ActionResult Index()
+        {
+            return View(db.tblUserType.ToList());
+        }
+
+        // GET: UserTypes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblUserType tblUserType = db.tblUserType.Find(id);
+            if (tblUserType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblUserType);
+        }
+
+        // GET: UserTypes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserTypes/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в разделе https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "UserTypeID,UserType")] tblUserType tblUserType)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tblUserType.Add(tblUserType);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tblUserType);
+        }
+
+        // GET: UserTypes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblUserType tblUserType = db.tblUserType.Find(id);
+            if (tblUserType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblUserType);
+        }
+
+        // POST: UserTypes/Edit/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в разделе https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "UserTypeID,UserType")] tblUserType tblUserType)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tblUserType).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tblUserType);
+        }
+
+        // GET: UserTypes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblUserType tblUserType = db.tblUserType.Find(id);
+            if (tblUserType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblUserType);
+        }
+
+        // POST: UserTypes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tblUserType tblUserType = db.tblUserType.Find(id);
+            db.tblUserType.Remove(tblUserType);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
