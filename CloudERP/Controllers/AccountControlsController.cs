c7d88839f5ef561f5dadcf87472152dd7ad11419
Somewhere <66011798+Kuvaev-dev@users.noreply.github.com>
2023-02-23 20:34:48 +0200

    Account Heads Added

---
+++ CloudERP/Controllers/AccountControlsController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DatabaseAccess;
+
+namespace CloudERP.Controllers
+{
+    public class AccountControlsController : Controller
+    {
+        private CloudDBEntities db = new CloudDBEntities();
+
+        // GET: AccountControls
+        public ActionResult Index()
+        {
+            var tblAccountControl = db.tblAccountControl.Include(t => t.tblBranch).Include(t => t.tblUser).Include(t => t.tblCompany);
+            return View(tblAccountControl.ToList());
+        }
+
+        // GET: AccountControls/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblAccountControl tblAccountControl = db.tblAccountControl.Find(id);
+            if (tblAccountControl == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblAccountControl);
+        }
+
+        // GET: AccountControls/Create
+        public ActionResult Create()
+        {
+            ViewBag.BranchID = new SelectList(db.tblBranch, "BranchID", "BranchName");
+            ViewBag.UserID = new SelectList(db.tblUser, "UserID", "FullName");
+            ViewBag.CompanyID = new SelectList(db.tblCompany, "CompanyID", "Name");
+            return View();
+        }
+
+        // POST: AccountControls/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в разделе https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "AccountControlID,CompanyID,BranchID,AccountHeadID,AccountControlName,UserID")] tblAccountControl tblAccountControl)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tblAccountControl.Add(tblAccountControl);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.BranchID = new SelectList(db.tblBranch, "BranchID", "BranchName", tblAccountControl.BranchID);
+            ViewBag.UserID = new SelectList(db.tblUser, "UserID", "FullName", tblAccountControl.UserID);
+            ViewBag.CompanyID = new SelectList(db.tblCompany, "CompanyID", "Name", tblAccountControl.CompanyID);
+            return View(tblAccountControl);
+        }
+
+        // GET: AccountControls/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblAccountControl tblAccountControl = db.tblAccountControl.Find(id);
+            if (tblAccountControl == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.BranchID = new SelectList(db.tblBranch, "BranchID", "BranchName", tblAccountControl.BranchID);
+            ViewBag.UserID = new SelectList(db.tblUser, "UserID", "FullName", tblAccountControl.UserID);
+            ViewBag.CompanyID = new SelectList(db.tblCompany, "CompanyID", "Name", tblAccountControl.CompanyID);
+            return View(tblAccountControl);
+        }
+
+        // POST: AccountControls/Edit/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в разделе https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "AccountControlID,CompanyID,BranchID,AccountHeadID,AccountControlName,UserID")] tblAccountControl tblAccountControl)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tblAccountControl).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.BranchID = new SelectList(db.tblBranch, "BranchID", "BranchName", tblAccountControl.BranchID);
+            ViewBag.UserID = new SelectList(db.tblUser, "UserID", "FullName", tblAccountControl.UserID);
+            ViewBag.CompanyID = new SelectList(db.tblCompany, "CompanyID", "Name", tblAccountControl.CompanyID);
+            return View(tblAccountControl);
+        }
+
+        // GET: AccountControls/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblAccountControl tblAccountControl = db.tblAccountControl.Find(id);
+            if (tblAccountControl == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblAccountControl);
+        }
+
+        // POST: AccountControls/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tblAccountControl tblAccountControl = db.tblAccountControl.Find(id);
+            db.tblAccountControl.Remove(tblAccountControl);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CloudERP/Controllers/AccountHeadsController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DatabaseAccess;
+
+namespace CloudERP.Controllers
+{
+    public class AccountHeadsController : Controller
+    {
+        private CloudDBEntities db = new CloudDBEntities();
+
+        // GET: AccountHeads
+        public ActionResult Index()
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["CompanyID"])))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+
+            int companyID = 0;
+            int branchID = 0;
+            int userID = 0;
+
+            companyID = Convert.ToInt32(Convert.ToString(Session["CompanyID"]));
+            branchID = Convert.ToInt32(Convert.ToString(Session["BranchID"]));
+            userID = Convert.ToInt32(Convert.ToString(Session["UserID"]));
+
+            var tblAccountHead = db.tblAccountHead.Include(t => t.tblUser)
+                                                  .Where(a => a.CompanyID == companyID && a.BranchID == branchID);
+            return View(tblAccountHead.ToList());
+        }
+
+        // GET: AccountHeads/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblAccountHead tblAccountHead = db.tblAccountHead.Find(id);
+            if (tblAccountHead == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblAccountHead);
+        }
+
+        // GET: AccountHeads/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AccountHeads/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в разделе https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(tblAccountHead tblAccountHead)
+        {
+            int companyID = 0;
+            int branchID = 0;
+            int userID = 0;
+
+            companyID = Convert.ToInt32(Convert.ToString(Session["CompanyID"]));
+            branchID = Convert.ToInt32(Convert.ToString(Session["BranchID"]));
+            userID = Convert.ToInt32(Convert.ToString(Session["UserID"]));
+
+            tblAccountHead.BranchID = branchID;
+            tblAccountHead.CompanyID = companyID;
+            tblAccountHead.UserID = userID;
+
+            if (ModelState.IsValid)
+            {
+                var findHead = db.tblAccountHead.Where(a => a.CompanyID == companyID
+                                                         && a.BranchID == branchID
+                                                         && a.AccountHeadName == tblAccountHead.AccountHeadName).FirstOrDefault();
+                if (findHead == null)
+                {
+                    db.tblAccountHead.Add(tblAccountHead);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ViewBag.Message = "Already Exist";
+                }
+            }
+
+            return View(tblAccountHead);
+        }
+
+        // GET: AccountHeads/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblAccountHead tblAccountHead = db.tblAccountHead.Find(id);
+            if (tblAccountHead == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UserID = new SelectList(db.tblUser, "UserID", "FullName", tblAccountHead.UserID);
+            return View(tblAccountHead);
+        }
+
+        // POST: AccountHeads/Edit/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в разделе https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "AccountHeadID,CompanyID,BranchID,AccountHeadName,Code,UserID")] tblAccountHead tblAccountHead)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tblAccountHead).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserID = new SelectList(db.tblUser, "UserID", "FullName", tblAccountHead.UserID);
+            return View(tblAccountHead);
+        }
+
+        // GET: AccountHeads/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblAccountHead tblAccountHead = db.tblAccountHead.Find(id);
+            if (tblAccountHead == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblAccountHead);
+        }
+
+        // POST: AccountHeads/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tblAccountHead tblAccountHead = db.tblAccountHead.Find(id);
+            db.tblAccountHead.Remove(tblAccountHead);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
